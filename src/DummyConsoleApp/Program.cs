c2198088f6b362dcfd566af006be411ff326d134
rogine ceniza <rogineceniza2018@gmail.com>
2023-02-01 23:54:06 +0800

    fgdfg

--- src/DummyConsoleApp/Program.cs
+++ src/DummyConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("First commit");
-Console.WriteLine("Ate Rogine");
+Console.WriteLine("Rogine");
+
