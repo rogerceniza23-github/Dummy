d020585d04523256da61591b9db49c7a77b30bf4
rogine ceniza <rogineceniza2018@gmail.com>
2023-02-02 19:18:40 +0800

    Random name picker

---
+++ HelloWorld/main.py
@@ -0,0 +1,11 @@
+# import random
+#
+# #Random Name Picker
+# import random
+# names_string = input("Give me everybody's names separated by a comma: ")
+# names = names_string.split(",")
+# num_items = (len(names))
+# myRandomNames = random.randint(0, num_items - 1)
+# person = names[myRandomNames]
+# print(f"Person who will play is {person}")
+# #print(random.choice(toSplit))
